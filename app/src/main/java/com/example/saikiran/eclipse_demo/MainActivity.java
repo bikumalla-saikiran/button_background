a775420c0271af61336648e6411c4693fe53bcbe
bikumalla saikiran <saikiranb34@gmail.com>
2019-03-29 20:59:42 +0530

    Initial commit

---
+++ app/src/main/java/com/example/saikiran/eclipse_demo/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.saikiran.eclipse_demo;
+
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button b= (Button)findViewById(R.id.button);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onButtonClick(View v)
+    {
+        b.setBackgroundResource(R.drawable.saikiran);
+    }
+}
